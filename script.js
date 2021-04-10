daeba78810bfff71ad663daee694b2e13827ee01
Dulfite <61243073+Dulfite@users.noreply.github.com>
2021-04-10 08:34:05 -0500

    Initial script and helper js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-// Write your JavaScript code here!
+window.addEventListener("load", function () {
+    let form = document.querySelector("form");
+    document.getElementById("faultyItems").style.visibility = "hidden"
+    form.addEventListener("submit", formSubmission);
+    let listedPlanets;
+    let listedPlanetsResponse = myFetch();
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        const planet = pickPlanet(listedPlanets);
+        addDestinationInfo(`${planet.name}, ${planet.diameter}, ${planet.star}, ${planet.distance}, ${planet.moons}, ${planet.image}`)
+    })
 
-window.addEventListener("load", function() {
-
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
 });
